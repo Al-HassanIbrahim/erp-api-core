61fa93f7ff0fe7721569b7a14ce56b2ec4e4da73
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-04 08:57:27 +0200

    ERPSystem.Infrastructure : Data : Add DbSet properties for Product, Category, and UnitOfMeasure entities

--- ERPSystem.Infrastructure/Data/AppDbContext.cs
+++ ERPSystem.Infrastructure/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Products;
 using Microsoft.EntityFrameworkCore;
 
 namespace ERPSystem.Infrastructure.Data
@@ -13,5 +14,9 @@ namespace ERPSystem.Infrastructure.Data
        : base(options)
         {
         }
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<UnitOfMeasure> UnitsOfMeasure => Set<UnitOfMeasure>();
+
     }
 }
