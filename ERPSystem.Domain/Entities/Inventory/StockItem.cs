8da467fea13a5e0031b35be543c053eabb7e6fc6
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 22:25:00 +0200

    ERPSystem.Domain : Entities : Inventory : Add StockItem class

---
+++ ERPSystem.Domain/Entities/Inventory/StockItem.cs
@@ -0,0 +1,21 @@
+﻿using ERPSystem.Domain.Abstractions;
+using ERPSystem.Domain.Entities.Products;
+
+namespace ERPSystem.Domain.Entities.Inventory
+{
+    public class StockItem : BaseEntity
+    {
+        public int WarehouseId { get; set; }
+        public int ProductId { get; set; }
+
+        public decimal QuantityOnHand { get; set; }
+        public decimal? MinQuantity { get; set; }   
+        public decimal? MaxQuantity { get; set; }  
+
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+
+        
+        public Warehouse Warehouse { get; set; } = default!;
+        public Product Product { get; set; } = default!;
+    }
+}
