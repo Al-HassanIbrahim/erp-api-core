4c8e33cd07fb91254c8f9751a2d5933795798ca8
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 16:21:34 +0200

    ERPSystem.Application : DTOs : Implement UpdateProductDto

--- ERPSystem.Application/DTOs/UpdateProductDto.cs
+++ ERPSystem.Application/DTOs/UpdateProductDto.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ERPSystem.Application.DTOs
+﻿namespace ERPSystem.Application.DTOs
 {
-    internal class UpdateProductDto
+    public class UpdateProductDto
     {
+        public int Id { get; set; }
+
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+
+        public int? CategoryId { get; set; }
+        public int UnitOfMeasureId { get; set; }
+
+        public decimal DefaultPrice { get; set; }
+        public string? Barcode { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
