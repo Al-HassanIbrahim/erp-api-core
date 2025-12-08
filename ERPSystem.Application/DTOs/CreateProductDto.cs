fa8fa30b6152f4692b8b7cbfbeb39a581ac8b863
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 16:19:55 +0200

    ERPSystem.Application : DTOs : Implement CreateProductDto

--- ERPSystem.Application/DTOs/CreateProductDto.cs
+++ ERPSystem.Application/DTOs/CreateProductDto.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ERPSystem.Application.DTOs
+﻿namespace ERPSystem.Application.DTOs
 {
-    internal class CreateProductDto
+    public class CreateProductDto
     {
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+
+        public int? CategoryId { get; set; }
+        public int UnitOfMeasureId { get; set; }
+
+        public decimal DefaultPrice { get; set; }
+        public string? Barcode { get; set; }
     }
 }
