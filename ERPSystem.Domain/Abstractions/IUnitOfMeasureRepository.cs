7a9d58e3d383855020f5bee72b740d370e4984d4
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 14:40:19 +0200

    ERPSystem.Domain : Abstractions : Add Interface IUnitOfMeasureRepository

--- ERPSystem.Domain/Abstractions/IUnitOfMeasureRepository.cs
+++ ERPSystem.Domain/Abstractions/IUnitOfMeasureRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Products;
 
 namespace ERPSystem.Domain.Abstractions
 {
-    internal interface IUnitOfMeasureRepository
+    public interface IUnitOfMeasureRepository
     {
+        Task<UnitOfMeasure?> GetByIdAsync(int id);
+        Task<IEnumerable<UnitOfMeasure>> GetAllAsync();
+        Task AddAsync(UnitOfMeasure unitOfMeasure);
+        Task UpdateAsync(UnitOfMeasure unitOfMeasure);
+        Task DeleteAsync(int id);
     }
 }
