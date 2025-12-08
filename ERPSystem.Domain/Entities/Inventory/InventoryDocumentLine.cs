69bc58a6d528b0541da7dc1cc04faea8f67c74d4
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 22:26:00 +0200

    ERPSystem.Domain : Entities : Inventory : Add InventoryDocumentLine class

---
+++ ERPSystem.Domain/Entities/Inventory/InventoryDocumentLine.cs
@@ -0,0 +1,25 @@
+﻿using ERPSystem.Domain.Abstractions;
+using ERPSystem.Domain.Entities.Products;
+using ERPSystem.Domain.Enums;
+
+namespace ERPSystem.Domain.Entities.Inventory
+{
+    public class InventoryDocumentLine : BaseEntity
+    {
+        public int InventoryDocumentId { get; set; }
+        public int ProductId { get; set; }
+
+        public int WarehouseId { get; set; }     
+        public decimal Quantity { get; set; }    
+        public int UnitId { get; set; }
+
+        public InventoryLineType LineType { get; set; }  // In | Out
+
+        public decimal? UnitCost { get; set; }
+        public string? Notes { get; set; }
+
+        public InventoryDocument Document { get; set; } = default!;
+        public Product Product { get; set; } = default!;
+        public Warehouse Warehouse { get; set; } = default!;
+    }
+}
