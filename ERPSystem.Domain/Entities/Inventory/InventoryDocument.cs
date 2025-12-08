5bdcd694a435478bfe070cd4ca32a32cd546e427
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 22:26:29 +0200

    ERPSystem.Domain : Entities : Inventory : Add InventoryDocument class

---
+++ ERPSystem.Domain/Entities/Inventory/InventoryDocument.cs
@@ -0,0 +1,30 @@
+﻿using ERPSystem.Domain.Abstractions;
+using ERPSystem.Domain.Enums;
+
+namespace ERPSystem.Domain.Entities.Inventory
+{
+    public class InventoryDocument : BaseEntity
+    {
+        public int CompanyId { get; set; }
+        public int? BranchId { get; set; }
+
+        public string DocNumber { get; set; } = default!;
+        public DateTime DocDate { get; set; }
+
+        public InventoryDocType DocType { get; set; }
+        public InventoryDocumentStatus Status { get; set; } = InventoryDocumentStatus.Draft;
+
+        public int? DefaultWarehouseId { get; set; }
+        public string? Notes { get; set; }
+
+        public int CreatedByUserId { get; set; }
+        public int? PostedByUserId { get; set; }
+        public DateTime? PostedAt { get; set; }
+
+        public string? SourceType { get; set; }
+        public int? SourceId { get; set; }
+
+        public Warehouse? DefaultWarehouse { get; set; }
+        public ICollection<InventoryDocumentLine> Lines { get; set; } = new List<InventoryDocumentLine>();
+    }
+}
