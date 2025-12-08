8f93ee97ecd2c155e38d0ef12f360c114aa9b39c
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 22:31:52 +0200

    ERPSystem.Infrastructure : Data : Add (warehouse,stockitem,inventorydocument,inventorydocumentline) DbSets at AppDBContext class

--- ERPSystem.Infrastructure/Data/AppDbContext.cs
+++ ERPSystem.Infrastructure/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Inventory;
 using ERPSystem.Domain.Entities.Products;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,10 @@ namespace ERPSystem.Infrastructure.Data
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Category> Categories => Set<Category>();
         public DbSet<UnitOfMeasure> UnitsOfMeasure => Set<UnitOfMeasure>();
+        public DbSet<Warehouse> Warehouses => Set<Warehouse>();
+        public DbSet<StockItem> StockItems => Set<StockItem>();
+        public DbSet<InventoryDocument> InventoryDocuments => Set<InventoryDocument>();
+        public DbSet<InventoryDocumentLine> InventoryDocumentLines => Set<InventoryDocumentLine>();
 
     }
 }
