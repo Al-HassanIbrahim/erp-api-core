d413a970b91f671aefe244219edf46ca2019001a
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 16:20:50 +0200

    ERPSystem.Application : DTOs : Implement ProductDto

--- ERPSystem.Application/DTOs/ProductDto.cs
+++ ERPSystem.Application/DTOs/ProductDto.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ERPSystem.Application.DTOs
+﻿namespace ERPSystem.Application.DTOs
 {
-    internal class ProductDto
+    public class ProductDto
     {
+        public int Id { get; set; }
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public decimal DefaultPrice { get; set; }
+
+        public string? CategoryName { get; set; }
+        public string UnitOfMeasureName { get; set; } = default!;
+
+        public bool IsActive { get; set; }
     }
 }
