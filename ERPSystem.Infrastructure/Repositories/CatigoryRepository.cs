fd4d63b4c473e0ada010abe094c546d1b10ff0b3
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 14:49:55 +0200

    ERPSystem.Infrastructure : Repositories : Add Product, Category, and UnitOfMeasure repository implementations

---
+++ ERPSystem.Infrastructure/Repositories/CatigoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Domain.Abstractions;
+using ERPSystem.Domain.Entities.Products;
+using ERPSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPSystem.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories
+                .Include(c => c.Products)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ERPSystem.Infrastructure/Repositories/UnitOfMeasureRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Domain.Abstractions;
+using ERPSystem.Domain.Entities.Products;
+using ERPSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPSystem.Infrastructure.Repositories
+{
+    public class UnitOfMeasureRepository : IUnitOfMeasureRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfMeasureRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UnitOfMeasure?> GetByIdAsync(int id)
+        {
+            return await _context.UnitsOfMeasure.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<UnitOfMeasure>> GetAllAsync()
+        {
+            return await _context.UnitsOfMeasure.ToListAsync();
+        }
+
+        public async Task AddAsync(UnitOfMeasure unitOfMeasure)
+        {
+            await _context.UnitsOfMeasure.AddAsync(unitOfMeasure);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(UnitOfMeasure unitOfMeasure)
+        {
+            _context.UnitsOfMeasure.Update(unitOfMeasure);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var unitOfMeasure = await _context.UnitsOfMeasure.FindAsync(id);
+            if (unitOfMeasure != null)
+            {
+                _context.UnitsOfMeasure.Remove(unitOfMeasure);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
