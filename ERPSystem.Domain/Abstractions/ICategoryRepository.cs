b22efe7441af00e21de828bfa7e03bea383fa2d3
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 14:41:43 +0200

    ERPSystem.Domain : Abstractions : Add Interface ICategoryRepository

--- ERPSystem.Domain/Abstractions/ICategoryRepository.cs
+++ ERPSystem.Domain/Abstractions/ICategoryRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Products;
 
 namespace ERPSystem.Domain.Abstractions
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<Category?> GetByIdAsync(int id);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
     }
 }
