18e1056788714dcbadd2889fec8bd284959b37a0
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-08 22:39:31 +0200

    ERPSystem.Domain : Enums : Add Inventory Enums

---
+++ ERPSystem.Domain/Enums/IvertoryEnums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Domain.Enums
+{
+    public enum InventoryDocType
+    {
+        In = 1,         
+        Out = 2,        
+        Transfer = 3,   
+        Adjustment = 4,  
+        Opening = 5     
+    }
+
+    public enum InventoryDocumentStatus
+    {
+        Draft = 1,   
+        Posted = 2,
+        Canceled = 3 
+    }
+
+    public enum InventoryLineType
+    {
+        In = 1,
+        Out = 2
+    }
+}
