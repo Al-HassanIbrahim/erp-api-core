6fe48467333cce5e82118ac36d46d9d7cfef4755
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2025-12-09 11:14:39 +0200

    ERPSystem.Application : DTOs : Inventory : Add WerehouseDTO

---
+++ ERPSystem.Application/DTOs/Inventory/WerehouseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.Inventory
+{
+    public class WarehouseDto
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Address { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
