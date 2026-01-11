99d877a84b6d6d307f067a375d89e167cdca847d
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-11 22:06:41 +0200

    Application : Interfaces : Add IInventoryService

---
+++ ERPSystem.Application/Interfaces/IInventoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Application.DTOs.Inventory;
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IInventoryService
+    {
+        Task<InventoryDocumentResponse> StockInAsync(StockInRequest request, CancellationToken cancellationToken = default);
+        Task<InventoryDocumentResponse> StockOutAsync(StockOutRequest request, CancellationToken cancellationToken = default);
+        Task<InventoryDocumentResponse> TransferAsync(StockTransferRequest request, CancellationToken cancellationToken = default);
+        Task<InventoryDocumentResponse> OpeningBalanceAsync(OpeningBalanceRequest request, CancellationToken cancellationToken = default);
+        Task<InventoryDocumentResponse> AdjustmentAsync(StockAdjustmentRequest request, CancellationToken cancellationToken = default);
+    }
+}
