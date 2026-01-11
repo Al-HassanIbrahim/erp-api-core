0d140382e0c6260da73486801cb84897bf7eece1
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-11 22:38:26 +0200

    Program.cs : register warehouse repository and service

--- ERPSyatem.API/Program.cs
+++ ERPSyatem.API/Program.cs
@@ -1,10 +1,12 @@
 
 using ERPSystem.Application.Interfaces;
 using ERPSystem.Application.Services;
+using ERPSystem.Application.Services.Inventory;
 using ERPSystem.Domain.Abstractions;
 using ERPSystem.Infrastructure.Data;
 using ERPSystem.Infrastructure.Repositories;
 using ERPSystem.Infrastructure.Repositories.ERPSystem.Infrastructure.Repositories;
+using ERPSystem.Infrastructure.Repositories.Inventory;
 using Microsoft.EntityFrameworkCore;
 
 namespace ERPSyatem.API
@@ -19,15 +21,30 @@ namespace ERPSyatem.API
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            //Product
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
             builder.Services.AddScoped<IUnitOfMeasureRepository, UnitOfMeasureRepository>();
             builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
             builder.Services.AddScoped<IProductService, ProductService>();
 
+            //Inventory
+            builder.Services.AddScoped<IWarehouseRepository, WarehouseRepository>();
+
+            builder.Services.AddScoped<IWarehouseService, WarehouseService>();
+
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); 
+                });
+            });
 
             var app = builder.Build();
 
@@ -38,6 +55,7 @@ namespace ERPSyatem.API
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("MyPolicy");
             app.UseAuthorization();
 
 
