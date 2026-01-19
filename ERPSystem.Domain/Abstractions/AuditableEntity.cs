354ed2e65f43c7f5ed6ad7cb4e2b1379f59607ed
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-19 04:48:05 +0200

    refactor(domain): make BaseEntity abstract and introduce AuditableEntity
    - Mark BaseEntity as abstract
    - Add AuditableEntity with user audit fields (CreatedByUserId, UpdatedByUserId, DeletedByUserId)
    - Prepare domain for identity-based auditing

---
+++ ERPSystem.Domain/Abstractions/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Domain.Abstractions
+{
+    public abstract class AuditableEntity : BaseEntity
+    {
+        public Guid CreatedByUserId { get; set; }
+
+        public Guid? UpdatedByUserId { get; set; }
+
+        public Guid? DeletedByUserId { get; set; }
+    }
+}
