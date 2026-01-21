28a5332b21ab05a98c742162e9925a19e7030cd9
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-21 03:20:31 +0200

    Infrastructure add ApplicationUser

---
+++ ERPSystem.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Core;
+using Microsoft.AspNetCore.Identity;
+
+namespace ERPSystem.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid> 
+    {
+        public int CompanyId { get; set; }
+        public Company Company { get; set; } = default!;
+    }
+}
