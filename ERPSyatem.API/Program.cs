5c42c1a345b5bc018d748886defcd294cfd31bbc
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-21 03:28:19 +0200

    Program.cs : Add security module handle identity and jwt

--- ERPSyatem.API/Program.cs
+++ ERPSyatem.API/Program.cs
@@ -1,13 +1,18 @@
 
+using System.Text;
 using ERPSystem.Application.Interfaces;
 using ERPSystem.Application.Services;
 using ERPSystem.Application.Services.Inventory;
 using ERPSystem.Domain.Abstractions;
 using ERPSystem.Infrastructure.Data;
+using ERPSystem.Infrastructure.Identity;
 using ERPSystem.Infrastructure.Repositories;
 using ERPSystem.Infrastructure.Repositories.ERPSystem.Infrastructure.Repositories;
 using ERPSystem.Infrastructure.Repositories.Inventory;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ERPSyatem.API
 {
@@ -40,6 +45,38 @@ namespace ERPSyatem.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            //Security 
+            builder.Services.AddIdentity<ApplicationUser,IdentityRole<Guid>>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication(options => //How to validate
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options=> //check if verified token
+            {
+                options.SaveToken = true;
+                //options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                   ValidateIssuer=true,
+                   ValidIssuer = builder.Configuration["JWT:IssuerIP"],
+
+                   ValidateAudience = true,
+                   ValidAudience = builder.Configuration["JWT:AudienceIP"],
+
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+                };
+            });
+
+            //CORS
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("MyPolicy", policy =>
@@ -58,6 +95,7 @@ namespace ERPSyatem.API
             }
 
             app.UseCors("MyPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
