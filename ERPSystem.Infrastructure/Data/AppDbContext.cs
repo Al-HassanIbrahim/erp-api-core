3b3b52bae13cb1b32f147fd2dea9a88a7d2ca552
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-21 03:23:35 +0200

    Infrastructure : Update AppDbContext with identity

--- ERPSystem.Infrastructure/Data/AppDbContext.cs
+++ ERPSystem.Infrastructure/Data/AppDbContext.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Core;
 using ERPSystem.Domain.Entities.Inventory;
 using ERPSystem.Domain.Entities.Products;
+using ERPSystem.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ERPSystem.Infrastructure.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
        : base(options)
         {
         }
+        public DbSet<Company> Companies => Set<Company>();
+        public DbSet<Module> Modules => Set<Module>();
+        public DbSet<CompanyModule> CompanyModules => Set<CompanyModule>();
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Category> Categories => Set<Category>();
         public DbSet<UnitOfMeasure> UnitsOfMeasure => Set<UnitOfMeasure>();
