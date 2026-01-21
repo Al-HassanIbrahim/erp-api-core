a6fe9491b5f71cad3d64006f9f9868b8bd438368
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-21 22:17:02 +0200

    Domain/Abstractions : Implement IModuleRepository

--- ERPSystem.Domain/Abstractions/IModuleRepository.cs
+++ ERPSystem.Domain/Abstractions/IModuleRepository.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Core;
 
 namespace ERPSystem.Domain.Abstractions
 {
-    internal interface IModuleRepository
+    public interface IModuleRepository
     {
+        Task<Module?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<Module?> GetByKeyAsync(string key, CancellationToken ct = default);
+        Task<List<Module>> GetAllAsync(CancellationToken ct = default);
+
+        Task AddAsync(Module module, CancellationToken ct = default);
+        void Update(Module module);
+
+        Task SoftDeleteAsync(int id, CancellationToken ct = default);
+
+        Task SaveChangesAsync(CancellationToken ct = default);
     }
 }
