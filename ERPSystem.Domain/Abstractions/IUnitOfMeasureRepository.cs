6fae705ed9b80e0f4fbb4d9b45d5406ad538db45
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-22 20:33:05 +0200

    Domain : Refactor IUnitOfMeasureRepository

--- ERPSystem.Domain/Abstractions/IUnitOfMeasureRepository.cs
+++ ERPSystem.Domain/Abstractions/IUnitOfMeasureRepository.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using ERPSystem.Domain.Entities.Products;
 
 namespace ERPSystem.Domain.Abstractions
 {
     public interface IUnitOfMeasureRepository
     {
-        Task<UnitOfMeasure?> GetByIdAsync(int id);
-        Task<IEnumerable<UnitOfMeasure>> GetAllAsync();
-        Task AddAsync(UnitOfMeasure unitOfMeasure);
-        Task UpdateAsync(UnitOfMeasure unitOfMeasure);
-        Task DeleteAsync(int id);
+        Task<UnitOfMeasure?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<List<UnitOfMeasure>> GetAllByCompanyAsync(int companyId, CancellationToken cancellationToken = default);
+        Task AddAsync(UnitOfMeasure unitOfMeasure, CancellationToken cancellationToken = default);
+        void Update(UnitOfMeasure unitOfMeasure);
+        void Delete(UnitOfMeasure unitOfMeasure);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
