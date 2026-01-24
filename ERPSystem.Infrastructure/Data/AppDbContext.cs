1a4b9c7ca692b81c57e260ff5622e87883761012
Mahmoud921 <hoda1892003@gmail.com>
2026-01-24 03:50:21 +0200

    Infrastructure/Date : Add Hr models to AppDbContext

--- ERPSystem.Infrastructure/Data/AppDbContext.cs
+++ ERPSystem.Infrastructure/Data/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ERPSystem.Domain.Entities.Core;
+using ERPSystem.Domain.Entities.HR;
 using ERPSystem.Domain.Entities.Inventory;
 using ERPSystem.Domain.Entities.Products;
 using ERPSystem.Infrastructure.Identity;
@@ -29,6 +30,20 @@ namespace ERPSystem.Infrastructure.Data
         public DbSet<StockItem> StockItems => Set<StockItem>();
         public DbSet<InventoryDocument> InventoryDocuments => Set<InventoryDocument>();
         public DbSet<InventoryDocumentLine> InventoryDocumentLines => Set<InventoryDocumentLine>();
+
+        // Hr
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<EmployeeDocument> EmployeeDocuments => Set<EmployeeDocument>();
+        public DbSet<Department> Departments => Set<Department>();
+        public DbSet<JobPosition> JobPositions => Set<JobPosition>();
+        public DbSet<Attendance> Attendances => Set<Attendance>();
+        public DbSet<LeaveRequest> LeaveRequests => Set<LeaveRequest>();
+        public DbSet<LeaveAttachment> LeaveAttachments => Set<LeaveAttachment>();
+        public DbSet<LeaveBalance> leaveBalances => Set<LeaveBalance>();
+        public DbSet<Payroll> Payrolls => Set<Payroll>();
+        public DbSet<PayrollLineItem> PayrollLineItems => Set<PayrollLineItem>();
+
+
 
     }
 }
