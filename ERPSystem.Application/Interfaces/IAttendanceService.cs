a9e6365955018d2391826d3c7a909d7e8051cc6b
Mahmoud921 <hoda1892003@gmail.com>
2026-01-25 07:08:31 +0200

    Application/Interfaces : Add Employee, Attendance, Leave and Payroll Interfce Services

---
+++ ERPSystem.Application/Interfaces/IAttendanceService.cs
@@ -0,0 +1,23 @@
+﻿using ERPSystem.Application.DTOs.HR.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ERPSystem.Application.DTOs.HR.Attendance.Check;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IAttendanceService
+    {
+        Task<AttendanceDto> CheckInAsync(CheckInDto dto, string createdBy);
+
+        Task<AttendanceDto> CheckOutAsync(CheckOutDto dto, string modifiedBy);
+
+        Task<AttendanceDto> CreateManualEntryAsync(ManualAttendanceDto dto, string createdBy);
+
+        Task<AttendanceDto> UpdateAsync(Guid id, UpdateAttendanceDto dto, string modifiedBy);
+
+        Task<AttendanceSummaryDto> GetSummaryAsync(Guid employeeId, int month, int year);
+    }
+}
---
+++ ERPSystem.Application/Interfaces/IEmployeeService.cs
@@ -0,0 +1,29 @@
+﻿using ERPSystem.Application.DTOs.HR.Employee;
+using ERPSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Task<EmployeeDetailDto> CreateAsync(CreateEmployeeDto dto, string createdBy);
+
+        Task<EmployeeDetailDto> UpdateAsync(Guid id, UpdateEmployeeDto dto, string modifiedBy);
+
+        Task UpdateStatusAsync(Guid id, UpdateEmployeeDto dto, string modifiedBy);
+
+        Task<EmployeeDetailDto?> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<EmployeeListDto>> GetAllAsync();
+
+        Task<IEnumerable<EmployeeListDto>> GetByDepartmentAsync(Guid departmentId);
+
+        Task<IEnumerable<EmployeeListDto>> GetByStatusAsync(EmployeeStatus status);
+
+        Task DeleteAsync(Guid id);
+    }
+}
