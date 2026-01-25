c5856d44e73117f8b8fa1584e98583df7ed07df0
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:11:46 +0200

    Application : Add CompanyDTOs

---
+++ ERPSystem.Application/DTOs/Core/CompanyDTOs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.Core
+{
+    public class CompanyMeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? CommercialName { get; set; }
+        public string? Address { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class UpdateCompanyMeDto
+    {
+        public string Name { get; set; } = default!;
+        public string? CommercialName { get; set; }
+        public string? Address { get; set; }
+    }
+}
