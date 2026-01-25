8be32fccead8e53313fd33c34ace9660a3c6d43d
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:38:47 +0200

    Domain : Add SalesReceiptAllocation class

---
+++ ERPSystem.Domain/Entities/Sales/SalesReceiptAllocation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Domain.Abstractions;
+
+namespace ERPSystem.Domain.Entities.Sales
+{
+    public class SalesReceiptAllocation : BaseEntity
+    {
+        public int SalesReceiptId { get; set; }
+        public SalesReceipt SalesReceipt { get; set; } = default!;
+
+        public int SalesInvoiceId { get; set; }
+        public SalesInvoice SalesInvoice { get; set; } = default!;
+
+        public decimal AllocatedAmount { get; set; }
+    }
+}
