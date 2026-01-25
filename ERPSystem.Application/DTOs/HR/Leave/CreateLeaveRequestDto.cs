9e45d536423ce6e6f1ca7242478a162e018a0077
Mahmoud921 <hoda1892003@gmail.com>
2026-01-25 04:10:27 +0200

    Application/DTOs/HR : Add Leave Dto

---
+++ ERPSystem.Application/DTOs/HR/Leave/CreateLeaveRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using ERPSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.HR.Leave
+{
+    public class CreateLeaveRequestDto
+    {
+        [Required]
+        public Guid EmployeeId { get; set; }
+
+        [Required]
+        public LeaveType LeaveType { get; set; }
+
+        [Required]
+        public DateOnly StartDate { get; set; }
+
+        [Required]
+        public DateOnly EndDate { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Reason { get; set; } = null!;
+    }
+}
---
+++ ERPSystem.Application/DTOs/HR/Leave/UpdateLeaveRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.HR.Leave
+{
+    public class UpdateLeaveRequestDto
+    {
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        [MaxLength(500)]
+        public string? Reason { get; set; }
+    }
+}
