0b11c673b638c3d305320b293ce490fd0bbd21b8
Mahmoud921 <hoda1892003@gmail.com>
2026-01-25 04:20:59 +0200

    Application/DTOs/HR : Add Payroll Dto

---
+++ ERPSystem.Application/DTOs/HR/Payroll/MarkPaidDto.cs
@@ -0,0 +1,24 @@
+﻿using ERPSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.HR.Payroll
+{
+    public class MarkPaidDto
+    {
+        public DateOnly? PaidDate { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [MaxLength(100)]
+        public string? TransactionReference { get; set; }
+
+        [MaxLength(1000)]
+        public string? Notes { get; set; }
+    }
+}
---
+++ ERPSystem.Application/DTOs/HR/Payroll/UpdatePayrollDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.HR.Payroll
+{
+    public class UpdatePayrollDto
+    {
+        public List<PayrollItemDto>? Allowances { get; set; }
+        public List<PayrollItemDto>? Deductions { get; set; }
+
+        [MaxLength(1000)]
+        public string? Notes { get; set; }
+    }
+}
