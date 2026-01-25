0652dbe9a53c711a093d933c9499de66ce920f88
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 08:50:30 +0200

    Application : Add Customer DTOs

---
+++ ERPSystem.Application/DTOs/Sales/CustomerDTOs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.Sales
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? TaxNumber { get; set; }
+        public decimal CreditLimit { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class CreateCustomerRequest
+    {
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? TaxNumber { get; set; }
+        public decimal CreditLimit { get; set; }
+    }
+
+    public class UpdateCustomerRequest
+    {
+        public string Name { get; set; } = default!;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? TaxNumber { get; set; }
+        public decimal CreditLimit { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
