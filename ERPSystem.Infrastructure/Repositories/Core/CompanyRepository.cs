01cfd3c69815bb8896173013b1c133499aa12e2f
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 14:52:55 +0200

    Update(Infrastructare) : CompanyModuleRepository & CompanyRepository & ModuleRepository

--- ERPSystem.Infrastructure/Repositories/Core/CompanyRepository.cs
+++ ERPSystem.Infrastructure/Repositories/Core/CompanyRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ERPSystem.Domain.Abstractions;
+﻿using ERPSystem.Domain.Abstractions;
 using ERPSystem.Domain.Entities.Core;
 using ERPSystem.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +21,12 @@ namespace ERPSystem.Infrastructure.Repositories.Core
                 .FirstOrDefaultAsync(c => c.Id == id && !c.IsDeleted, ct);
         }
 
+        public async Task<Company?> GetByIdTrackingAsync(int id, CancellationToken ct = default)
+        {
+            return await _context.Companies
+                .FirstOrDefaultAsync(c => c.Id == id && !c.IsDeleted, ct);
+        }
+
         public async Task<List<Company>> GetAllAsync(CancellationToken ct = default)
         {
             return await _context.Companies
@@ -33,6 +34,17 @@ namespace ERPSystem.Infrastructure.Repositories.Core
                 .Where(c => !c.IsDeleted)
                 .OrderByDescending(c => c.Id)
                 .ToListAsync(ct);
+        }
+
+        public async Task<bool> CodeExistsAsync(string code, int? excludeId = null, CancellationToken ct = default)
+        {
+            var query = _context.Companies
+                .Where(c => c.CommercialName == code && !c.IsDeleted);
+
+            if (excludeId.HasValue)
+                query = query.Where(c => c.Id != excludeId.Value);
+
+            return await query.AnyAsync(ct);
         }
 
         public async Task AddAsync(Company company, CancellationToken ct = default)
@@ -54,7 +66,6 @@ namespace ERPSystem.Infrastructure.Repositories.Core
 
             company.IsDeleted = true;
             company.DeletedByUserId = deletedByUserId;
-
             company.UpdatedAt = DateTime.UtcNow;
             company.UpdatedByUserId = deletedByUserId;
 
@@ -71,6 +82,4 @@ namespace ERPSystem.Infrastructure.Repositories.Core
             await _context.SaveChangesAsync(ct);
         }
     }
-
-
 }
