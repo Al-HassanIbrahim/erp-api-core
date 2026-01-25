64a01f7fd0c880026f503cfa10f9995714173ba3
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:25:55 +0200

    Domain : Add SalesInvoice class

---
+++ ERPSystem.Domain/Entities/Sales/SalesInvoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Domain.Abstractions;
+using ERPSystem.Domain.Enums;
+namespace ERPSystem.Domain.Entities.Sales
+{
+    public class SalesInvoice : AuditableEntity, ICompanyEntity
+    {
+        public int CompanyId { get; set; }
+        public int? BranchId { get; set; }
+
+        public string InvoiceNumber { get; set; } = default!;
+        public DateTime InvoiceDate { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = default!;
+
+        public SalesInvoiceStatus Status { get; set; } = SalesInvoiceStatus.Draft;
+        public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Unpaid;
+
+        public decimal SubTotal { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal GrandTotal { get; set; }
+        public decimal PaidAmount { get; set; }
+        public decimal BalanceDue => GrandTotal - PaidAmount;
+
+        public string? Notes { get; set; }
+
+        public Guid? PostedByUserId { get; set; }
+        public DateTime? PostedAt { get; set; }
+
+        public ICollection<SalesInvoiceLine> Lines { get; set; } = new List<SalesInvoiceLine>();
+        public ICollection<SalesDelivery> Deliveries { get; set; } = new List<SalesDelivery>();
+        public ICollection<SalesReceiptAllocation> ReceiptAllocations { get; set; } = new List<SalesReceiptAllocation>();
+    }
+}
