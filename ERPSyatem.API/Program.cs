50c535392ac8df564dd666832d67d7719b3959e0
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:22:11 +0200

    appsettings : update the expiratin time to 120M

--- ERPSyatem.API/Program.cs
+++ ERPSyatem.API/Program.cs
@@ -1,14 +1,18 @@
 using System.Text;
 using ERPSystem.Application.Interfaces;
-using ERPSystem.Application.Services;
+using ERPSystem.Application.Interfaces.ERPSystem.Application.Interfaces;
+using ERPSystem.Application.Services.Core;
 using ERPSystem.Application.Services.Inventory;
 using ERPSystem.Application.Services.Products;
+using ERPSystem.Application.Services.Sales;
 using ERPSystem.Domain.Abstractions;
 using ERPSystem.Infrastructure.Data;
 using ERPSystem.Infrastructure.Identity;
 using ERPSystem.Infrastructure.Repositories;
+using ERPSystem.Infrastructure.Repositories.Core;
 using ERPSystem.Infrastructure.Repositories.ERPSystem.Infrastructure.Repositories;
 using ERPSystem.Infrastructure.Repositories.Inventory;
+using ERPSystem.Infrastructure.Repositories.Sales;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -19,13 +23,25 @@ namespace ERPSyatem.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Core
+            builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+            builder.Services.AddScoped<IModuleRepository, ModuleRepository>();
+            builder.Services.AddScoped<ICompanyModuleRepository, CompanyModuleRepository>();
+            builder.Services.AddScoped<IModuleAccessService, ModuleAccessService>();
+
+
+            builder.Services.AddScoped<ICompanyProfileService, CompanyProfileService>();
+            builder.Services.AddScoped<IModuleService, ModuleService>();
+            builder.Services.AddScoped<ICompanyModuleService, CompanyModuleService>();
+            builder.Services.AddScoped<ICompanyUserService, CompanyUserService>();
 
             //Product
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
@@ -49,6 +65,20 @@ namespace ERPSyatem.API
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
+            // Sales
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+            builder.Services.AddScoped<ISalesInvoiceRepository, SalesInvoiceRepository>();
+            builder.Services.AddScoped<ISalesDeliveryRepository, SalesDeliveryRepository>();
+            builder.Services.AddScoped<ISalesReceiptRepository, SalesReceiptRepository>();
+            builder.Services.AddScoped<ISalesReturnRepository, SalesReturnRepository>();
+
+            builder.Services.AddScoped<ICustomerService, CustomerService>();
+            builder.Services.AddScoped<ISalesInvoiceService, SalesInvoiceService>();
+            builder.Services.AddScoped<ISalesDeliveryService, SalesDeliveryService>();
+            builder.Services.AddScoped<ISalesReceiptService, SalesReceiptService>();
+            builder.Services.AddScoped<ISalesReturnService, SalesReturnService>();
+
+
             #region Swagger
             builder.Services.AddSwaggerGen(c =>
             {
@@ -126,6 +156,12 @@ namespace ERPSyatem.API
             });
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                await DbSeeder.SeedModulesAsync(context);
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
