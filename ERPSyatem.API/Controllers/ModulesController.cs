6a7b2cd896ba7656a6b7af989637551ab932f196
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:17:34 +0200

    API : Add ModulesController to show only the Modules

---
+++ ERPSyatem.API/Controllers/ModulesController.cs
@@ -0,0 +1,35 @@
+﻿using ERPSystem.Application.DTOs.Core;
+using ERPSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPSyatem.API.Controllers
+{
+    [ApiController]
+    [Route("api/modules")]
+    public class ModulesController : ControllerBase
+    {
+        private readonly IModuleService _service;
+
+        public ModulesController(IModuleService service)
+        {
+            _service = service;
+        }
+
+        // Get all available modules
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken ct)
+        {
+            var result = await _service.GetAllAsync(ct);
+            return Ok(result);
+        }
+
+        // Create a new module (restricted to SystemAdmin)
+        //[HttpPost]
+        //public async Task<IActionResult> Create([FromBody] CreateModuleDto dto, CancellationToken ct)
+        //{
+        //    var result = await _service.CreateAsync(dto, ct);
+        //    return CreatedAtAction(nameof(GetAll), new { id = result.Id }, result);
+        //}
+    }
+}
