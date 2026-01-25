030bc9b8598dcb6ab7de255e2340a635f68995ab
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 16:00:18 +0200

    Realocate product DTOs

---
+++ ERPSystem.Application/DTOs/Products/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ERPSystem.Application.DTOs.Products
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public decimal DefaultPrice { get; set; }
+
+        public string? CategoryName { get; set; }
+        public string UnitOfMeasureName { get; set; } = default!;
+
+        public bool IsActive { get; set; }
+    }
+}
