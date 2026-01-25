cc03f2ff22b19463960c1ff597b8589dbe3c0908
Mahmoud921 <hoda1892003@gmail.com>
2026-01-25 06:59:32 +0200

    Application/DTOs/Hr/Attendance : Add Manual Attendance

---
+++ ERPSystem.Application/DTOs/HR/Attendance/ManualAttendanceDto.cs
@@ -0,0 +1,28 @@
+﻿using ERPSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.HR.Attendance
+{
+    public class ManualAttendanceDto
+    {
+        [Required]
+        public Guid EmployeeId { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        public TimeOnly? CheckInTime { get; set; }
+        public TimeOnly? CheckOutTime { get; set; }
+
+        [Required]
+        public AttendanceStatus Status { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Notes { get; set; } = null!;
+    }
+}
