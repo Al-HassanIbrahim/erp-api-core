a8c157799de15cf3ed15bd6f45f203dda9f1a546
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:12:07 +0200

    Application : Add CompanyModuleDTOs

---
+++ ERPSystem.Application/DTOs/Core/CompanyModuleDTOs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.DTOs.Core
+{
+    public class CompanyModuleDto
+    {
+        public int ModuleId { get; set; }
+        public string ModuleKey { get; set; } = default!;
+        public string ModuleName { get; set; } = default!;
+        public bool IsEnabled { get; set; }
+        public DateTime? EnabledAt { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+    }
+
+    public class ToggleCompanyModuleDto
+    {
+        public bool IsEnabled { get; set; }
+    }
+}
