fca0a95a7e8f8edc67010f858cda16d7323c1939
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-25 15:03:40 +0200

    Application : Add IModuleAccessService

---
+++ ERPSystem.Application/Interfaces/IModuleAccessService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    namespace ERPSystem.Application.Interfaces
+    {
+        public interface IModuleAccessService
+        {
+            Task<bool> IsModuleEnabledAsync(int companyId, string moduleCode, CancellationToken cancellationToken = default);
+            Task<bool> IsSalesEnabledAsync(CancellationToken cancellationToken = default);
+            Task<bool> IsInventoryEnabledAsync(CancellationToken cancellationToken = default);
+            Task EnsureSalesEnabledAsync(CancellationToken cancellationToken = default);
+            Task EnsureInventoryEnabledAsync(CancellationToken cancellationToken = default);
+        }
+    }
+}
