3be60e3294668895838d5fd7159e390c53b72477
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-26 09:26:35 +0200

    feat(Application) : ISalesReceiptService

---
+++ ERPSystem.Application/Interfaces/ISalesReceiptService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Application.DTOs.Sales;
+using ERPSystem.Domain.Enums;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface ISalesReceiptService
+    {
+        Task<IReadOnlyList<SalesReceiptListDto>> GetAllAsync(
+            int? customerId = null,
+            SalesReceiptStatus? status = null,
+            DateTime? fromDate = null,
+            DateTime? toDate = null,
+            CancellationToken cancellationToken = default);
+        Task<SalesReceiptDto?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<SalesReceiptDto> CreateAsync(CreateSalesReceiptRequest request, CancellationToken cancellationToken = default);
+        Task<SalesReceiptDto> PostAsync(int id, CancellationToken cancellationToken = default);
+        Task<SalesReceiptDto> CancelAsync(int id, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
