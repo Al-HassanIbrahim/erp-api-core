b37a72449ab9be2a068a16cdb827e95f83c0a03b
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-26 06:28:10 +0200

    feat(Aplication) : Add ICompanyUserService

---
+++ ERPSystem.Application/Interfaces/ICompanyUserService.cs
@@ -0,0 +1,12 @@
+﻿using ERPSystem.Application.DTOs.Core;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface ICompanyUserService
+    {
+        Task<IReadOnlyList<CompanyUserDto>> GetAllAsync(CancellationToken ct = default);
+        Task<CompanyUserDto> CreateAsync(CreateCompanyUserDto dto, CancellationToken ct = default);
+        Task<CompanyUserDto> UpdateRolesAsync(Guid userId, UpdateUserRolesDto dto, CancellationToken ct = default);
+        Task<CompanyUserDto> UpdateStatusAsync(Guid userId, UpdateUserStatusDto dto, CancellationToken ct = default);
+    }
+}
