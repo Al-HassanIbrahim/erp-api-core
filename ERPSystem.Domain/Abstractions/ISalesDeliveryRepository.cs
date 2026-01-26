5f373ce3db3f4a355cc2d8d54b207e1c0eba5cfc
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-26 09:50:02 +0200

    feat(Abstractions) : Add ISalesDeliveryRepository

---
+++ ERPSystem.Domain/Abstractions/ISalesDeliveryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Domain.Entities.Sales;
+using ERPSystem.Domain.Enums;
+
+namespace ERPSystem.Domain.Abstractions
+{
+    public interface ISalesDeliveryRepository
+    {
+        Task<SalesDelivery?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<SalesDelivery?> GetByIdWithLinesAsync(int id, CancellationToken cancellationToken = default);
+        Task<List<SalesDelivery>> GetAllByCompanyAsync(
+            int companyId,
+            int? invoiceId = null,
+            SalesDeliveryStatus? status = null,
+            DateTime? fromDate = null,
+            DateTime? toDate = null,
+            CancellationToken cancellationToken = default);
+        Task<string> GenerateDeliveryNumberAsync(int companyId, CancellationToken cancellationToken = default);
+        Task AddAsync(SalesDelivery delivery, CancellationToken cancellationToken = default);
+        void Update(SalesDelivery delivery);
+        void Delete(SalesDelivery delivery);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
