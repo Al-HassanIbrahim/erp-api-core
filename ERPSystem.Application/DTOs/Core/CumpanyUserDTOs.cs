ead619e17c29d7b526ccf5f5132b9ee9d67f4b5e
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-26 06:27:05 +0200

    feat(Application) : Add CumpanyUserDTOs

---
+++ ERPSystem.Application/DTOs/Core/CumpanyUserDTOs.cs
@@ -0,0 +1,30 @@
+﻿namespace ERPSystem.Application.DTOs.Core
+{
+    public class CompanyUserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = default!;
+        public string? FullName { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsLockedOut { get; set; }
+        public List<string> Roles { get; set; } = new();
+    }
+
+    public class CreateCompanyUserDto
+    {
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string? FullName { get; set; }
+        public List<string>? Roles { get; set; }
+    }
+
+    public class UpdateUserRolesDto
+    {
+        public List<string> Roles { get; set; } = new();
+    }
+
+    public class UpdateUserStatusDto
+    {
+        public bool IsActive { get; set; }
+    }
+}
