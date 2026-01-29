d2a6a07f58a58296b0def9e2466bb4ab2ee6eb72
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-29 12:36:43 +0200

    feat(Application) : Add IContactService

---
+++ ERPSystem.Application/Interfaces/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Application.DTOs.Contacts;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IContactService
+    {
+        Task<List<ContactDto>> GetAllAsync(CancellationToken cancellationToken);
+        Task<ContectDetailsDto?> GetContact(int Id,CancellationToken cancellationToken);
+        Task<ContactDto> Create(CreateContactRequest request,CancellationToken cancellationToken);
+        Task<ContactDto> Update(UpdateContactDto request,CancellationToken cancellationToken);
+        Task Delete(int id,CancellationToken cancellationToken);
+
+    }
+}
