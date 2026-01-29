c8fc855dcef4e49c7432264e177d5fca0527bf6f
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-30 01:00:40 +0200

    register Expenses Module in program.cs

--- ERPSyatem.API/Program.cs
+++ ERPSyatem.API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using ERPSystem.Application.Interfaces;
 using ERPSystem.Application.Services.Contacts;
 using ERPSystem.Application.Services.Core;
+using ERPSystem.Application.Services.Expenses;
 using ERPSystem.Application.Services.Inventory;
 using ERPSystem.Application.Services.Products;
 using ERPSystem.Application.Services.Sales;
@@ -11,6 +12,7 @@ using ERPSystem.Infrastructure.Identity;
 using ERPSystem.Infrastructure.Repositories;
 using ERPSystem.Infrastructure.Repositories.Contacts;
 using ERPSystem.Infrastructure.Repositories.Core;
+using ERPSystem.Infrastructure.Repositories.Expenses;
 using ERPSystem.Infrastructure.Repositories.Inventory;
 using ERPSystem.Infrastructure.Repositories.Sales;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -83,6 +85,13 @@ namespace ERPSyatem.API
            
             builder.Services.AddScoped<IContactService, ContactService>();
 
+            // Expenses
+            builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+            builder.Services.AddScoped<IExpenseCategoryRepository, ExpenseCategoryRepository>();
+            
+            builder.Services.AddScoped<IExpenseService, ExpenseService>();
+            builder.Services.AddScoped<IExpenseCategoryService, ExpenseCategoryService>();
+            builder.Services.AddScoped<IExpenseStatsService, ExpenseStatsService>();
 
             #region Swagger
             builder.Services.AddSwaggerGen(c =>
