7d92f918e3b90a15865fe5d87a764dd240f16468
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-01-29 12:33:35 +0200

    update(Domain) : Add ContactEnums

---
+++ ERPSystem.Domain/Enums/ContactEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Domain.Enums
+{
+    public enum ContactPersonType
+    {
+        Client = 1,
+        Vendor = 2,
+        Partner = 4,
+        leader = 5
+    }
+}
