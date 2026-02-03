3ab6d340e5c7e23a033ec0e458fb78f6ba5a98e3
Mahmoud921 <hoda1892003@gmail.com>
2026-02-04 01:28:11 +0200

    Infrastructure/Repository/Crm : Add Crm Repos

---
+++ ERPSystem.Infrastructure/Repositories/CRM/TenantGuard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Infrastructure.Repositories.CRM
+{
+    public static class TenantGuard
+    {
+        public static void EnsureCompany(int requestedCompanyId, int currentCompanyId)
+        {
+            if (requestedCompanyId != currentCompanyId)
+                throw new UnauthorizedAccessException("Cross-company access is not allowed.");
+        }
+    }
+}
