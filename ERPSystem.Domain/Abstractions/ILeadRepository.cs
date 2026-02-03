6d6fb9486429fc54ad9d4b25a8e4dbab38ddc54c
Mahmoud921 <hoda1892003@gmail.com>
2026-02-04 01:28:47 +0200

    Domain/Abstractios : Add ILeadRepo and PipelineRepo

---
+++ ERPSystem.Domain/Abstractions/ILeadRepository.cs
@@ -0,0 +1,29 @@
+﻿using ERPSystem.Domain.Entities.CRM;
+using ERPSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Domain.Abstractions
+{
+    public interface ILeadRepository
+    {
+        Task<Lead?> GetByIdAsync(int id, int companyId, CancellationToken ct = default);
+
+        Task<List<Lead>> ListAsync(
+            int companyId,
+            LeadStatus? stage = null,
+            LeadSource? source = null,
+            Guid? assignedToId = null,
+            string? search = null,
+            CancellationToken ct = default);
+
+        Task AddAsync(Lead lead, int companyId, CancellationToken ct = default);
+        Task UpdateAsync(Lead lead, int companyId);
+        Task DeleteAsync(int id, int companyId, CancellationToken ct = default);
+
+        Task SaveChangesAsync(CancellationToken ct = default);
+    }
+}
