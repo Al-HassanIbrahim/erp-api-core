6aded1cc3b702e643732e163958432e1238bc110
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 06:58:16 +0300

    feat(Domain): Add Purchasing Module Enums

---
+++ ERPSystem.Domain/Enums/PurchaseEnums.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Domain.Enums
+{
+    public enum PurchaseInvoiceStatus
+    {
+        Draft = 0,
+        Posted = 1,
+        Cancelled = 2,
+    }
+
+    public enum PurchasePaymentStatus
+    {
+        Unpaid = 0,
+        PartiallyPaid = 1,
+        Paid = 2,
+    }
+
+    public enum PurchaseReturnStatus
+    {
+        Draft = 0,
+        Posted = 1,
+        Cancelled = 2,
+    }
+
+    public enum SupplierPaymentStatus
+    {
+        Draft = 0,
+        Posted = 1,
+        Cancelled = 2,
+    }
+}
