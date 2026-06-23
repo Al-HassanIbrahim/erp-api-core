6ca63fd78730204aafd45c65506b315e20d01a40
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 07:19:16 +0300

    Add refactor-invoice-creation Migrations

---
+++ ERPSystem.Infrastructure/Migrations/20260620181006_refactor-invoice-creation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactorinvoicecreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence<int>(
+                name: "InventoryDocumentSeq");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "SalesDeliverySeq");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "SalesInvoiceSeq");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "SalesReceiptSeq");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "SalesReturnSeq");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropSequence(
+                name: "InventoryDocumentSeq");
+
+            migrationBuilder.DropSequence(
+                name: "SalesDeliverySeq");
+
+            migrationBuilder.DropSequence(
+                name: "SalesInvoiceSeq");
+
+            migrationBuilder.DropSequence(
+                name: "SalesReceiptSeq");
+
+            migrationBuilder.DropSequence(
+                name: "SalesReturnSeq");
+        }
+    }
+}
