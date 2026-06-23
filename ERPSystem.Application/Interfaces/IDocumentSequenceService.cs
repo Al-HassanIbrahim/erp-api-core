a6c1229ca4ccb5c345b33e89f61c63678215a130
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 05:45:54 +0300

    feat(interfaces): define IDocumentSequenceService for serial generation

---
+++ ERPSystem.Application/Interfaces/IDocumentSequenceService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IDocumentSequenceService
+    {
+        Task<string> GenerateNextNumberAsync(
+            int companyId,
+            string documentType,
+            string prefix,
+            CancellationToken ct = default);
+    }
+}
