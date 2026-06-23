b6c348d679839ce9b0d2e9bdc5e0e4b4415d856e
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 06:53:56 +0300

    feat(Interface): Add IPurchaseReturnService

---
+++ ERPSystem.Application/Interfaces/IPurchaseReturnService.cs
@@ -0,0 +1,18 @@
+﻿using ERPSystem.Application.DTOs.Purchase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IPurchaseReturnService
+    {
+        Task<IReadOnlyList<PurchaseReturnListDto>> GetAllAsync(CancellationToken ct = default);
+        Task<PurchaseReturnDto> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<PurchaseReturnDto> CreateAsync(CreatePurchaseReturnDto request, CancellationToken ct = default);
+        Task PostAsync(int id, CancellationToken ct = default);
+        Task DeleteAsync(int id, CancellationToken ct = default);
+    }
+}
