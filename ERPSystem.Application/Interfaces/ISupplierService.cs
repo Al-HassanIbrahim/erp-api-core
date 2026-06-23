daa27facbf7b04c983833beae094529a26bca9d9
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 06:51:50 +0300

    feat(Interface): Add ISupplierService

---
+++ ERPSystem.Application/Interfaces/ISupplierService.cs
@@ -0,0 +1,18 @@
+﻿using ERPSystem.Application.DTOs.Purchase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface ISupplierService
+    {
+        Task<IReadOnlyList<SupplierListDto>> GetAllAsync(CancellationToken ct = default);
+        Task<SupplierDto> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<SupplierDto> CreateAsync(CreateSupplierDto request, CancellationToken ct = default);
+        Task<SupplierDto> UpdateAsync(int id, UpdateSupplierDto request, CancellationToken ct = default);
+        Task DeleteAsync(int id, CancellationToken ct = default);
+    }
+}
