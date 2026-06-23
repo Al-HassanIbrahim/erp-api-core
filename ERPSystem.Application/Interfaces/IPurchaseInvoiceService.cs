6d6e83da0de6c41dae9924da5d0d12c901f7e1d6
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 06:51:05 +0300

    feat(Interface): Add IPurchaseInvoiceService

---
+++ ERPSystem.Application/Interfaces/IPurchaseInvoiceService.cs
@@ -0,0 +1,20 @@
+﻿using ERPSystem.Application.DTOs.Purchase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+
+    public interface IPurchaseInvoiceService
+    {
+        Task<IReadOnlyList<PurchaseInvoiceListDto>> GetAllAsync(CancellationToken ct = default);
+        Task<PurchaseInvoiceDto> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<PurchaseInvoiceDto> CreateAsync(CreatePurchaseInvoiceDto request, CancellationToken ct = default);
+        Task<PurchaseInvoiceDto> UpdateAsync(int id, UpdatePurchaseInvoiceDto request, CancellationToken ct = default);
+        Task PostAsync(int id, CancellationToken ct = default);
+        Task DeleteAsync(int id, CancellationToken ct = default);
+    }
+}
