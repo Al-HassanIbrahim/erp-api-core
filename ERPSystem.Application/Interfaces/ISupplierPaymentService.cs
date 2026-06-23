25e4362dc0afe19e515720f9ed7526a9108eb368
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 06:54:21 +0300

    feat(Interface): Add ISupplierPaymentService

---
+++ ERPSystem.Application/Interfaces/ISupplierPaymentService.cs
@@ -0,0 +1,18 @@
+﻿using ERPSystem.Application.DTOs.Purchase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface ISupplierPaymentService
+    {
+        Task<IReadOnlyList<SupplierPaymentListDto>> GetAllAsync(CancellationToken ct = default);
+        Task<SupplierPaymentDto> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<SupplierPaymentDto> CreateAsync(CreateSupplierPaymentDto request, CancellationToken ct = default);
+        Task PostAsync(int id, CancellationToken ct = default);
+        Task CancelAsync(int id, CancellationToken ct = default);
+    }
+}
