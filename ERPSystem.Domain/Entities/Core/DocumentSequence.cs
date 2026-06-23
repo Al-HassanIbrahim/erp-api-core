c8cb85096d0435a921608f78d644b4e321245a0a
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 06:19:27 +0300

    add DocumentSequence Entity

---
+++ ERPSystem.Domain/Entities/Core/DocumentSequence.cs
@@ -0,0 +1,18 @@
+﻿using ERPSystem.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Domain.Entities.Core
+{
+    public sealed class DocumentSequence : ICompanyEntity
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string DocumentType { get; set; } = string.Empty; // "SalesInvoice", "SalesDelivery", "InventoryDocument", etc.
+        public string YearMonth { get; set; } = string.Empty;    // "202606"
+        public int LastSequence { get; set; }
+    }
+}
