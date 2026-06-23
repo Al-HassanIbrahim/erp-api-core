c8411aee325d5bb09802df52e997a89d2195ede8
Al-Hassanibrahim <alhassanibrahim.dev01@gmail.com>
2026-06-23 05:47:50 +0300

    feat(interfaces): define IPdfExportService

---
+++ ERPSystem.Application/Interfaces/IPdfExportService.cs
@@ -0,0 +1,15 @@
+﻿using ERPSystem.Domain.Enums;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Application.Interfaces
+{
+    public interface IPdfExportService
+    {
+        Task<byte[]> GenerateSalesInvoicePdfAsync(int invoiceId, string lang, CancellationToken ct = default);
+        //Task<byte[]> GenerateSalesInvoicesBulkPdfAsync(int? customerId,SalesInvoiceStatus? status,DateTime? fromDate,DateTime? toDate,string lang,CancellationToken ct);
+        Task<byte[]> GenerateSalesDeliveryPdfAsync(int deliveryId, string lang, CancellationToken ct = default);
+        Task<byte[]> GenerateSalesReceiptPdfAsync(int receiptId, string lang, CancellationToken ct = default);
+        Task<byte[]> GenerateSalesReturnPdfAsync(int returnId, string lang, CancellationToken ct = default);
+    }
+}   
